6f984b3ace9f62b642cbdc7e89824df14cf60bb7
Jeetu Chauhan <jeetu.chauhan@cerner.com>
2019-03-17 16:49:08 -0500

    Chore: Installed react components using yarn and npm

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+
+
+const App: React.FC = () => {
+  return (
+<View><Text>Hello from React Native</Text></View>
+
+  );
+}
+  
+
+export default App;
